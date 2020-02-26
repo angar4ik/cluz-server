19beac793e0e39dd433d73e86bad034920d4d54d
Anatoly G <4angar@gmail.com>
2020-02-26 09:39:11 -0500

    web removed corrcetly

--- CLUZServer/Startup.cs
+++ CLUZServer/Startup.cs
@@ -23,7 +23,7 @@ namespace CLUZServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            //services.AddControllersWithViews();
 
             //services.AddControllers()
             //    .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
@@ -55,22 +55,22 @@ namespace CLUZServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            //app.UseHttpsRedirection();
             //app.UseStaticFiles();
 
-            //app.UseRouting();
+            app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             //app.UseEndpoints(endpoints =>
             //{
